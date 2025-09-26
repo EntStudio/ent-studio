ed4aa5fb8ca435fffca46f3541c4b861ad963594
CouldBeAncient <chebotaaaaaa@gmail.com>
2025-09-26 20:51:02 +0900

    feat: add entry load event

--- src/content/main/workspace.ts
+++ src/content/main/workspace.ts
@@ -1,34 +1,53 @@
-function findEntryJS() {
-  for (const script of document.scripts) {
-    if (!script.src)
-      continue
+import { sleep } from "src/utils/utils.ts"
 
-    try {
-      const url = new URL(script.src)
-      const filename = url.pathname.split('/').pop() ?? ''
-
-      if (!(['entry.min.js', 'entry.js'].includes(filename)))
+async function findEntryJS(attempts: number = 3, timeout: number = 3) {
+  for (let i = attempts; i > 0; i--) {
+    for (const script of document.scripts) {
+      if (!script.src)
         continue
 
-      return {
-        script,
-        version: url.searchParams.get('v')
+      try {
+        const url = new URL(script.src)
+        const filename = url.pathname.split('/').pop() ?? ''
+
+        if (!(['entry.min.js', 'entry.js'].includes(filename)))
+          continue
+
+        return {
+          script,
+          version: url.searchParams.get('v')
+        }
+
+
+      } catch (e) {
+        continue
       }
+    }
 
-
-    } catch (e) {
-      continue
+    if (i > 1) {
+      console.warn("EntryJS를 찾지 못했습니다. 다시 시도하는 중...")
+      await sleep(timeout)
     }
   }
 
   throw new Error("EntryJS를 찾지 못했습니다")
 }
 
-const entryInfo = findEntryJS()
+let entryInfo = await findEntryJS()
 
 console.log(`entry.js 확인 완료 (버전 ${entryInfo.version ?? '알 수 없음'})`)
 
-entryInfo.script.addEventListener('load', () => {
-  // WIP
-  console.log('entry 객체', window.Entry)
-})
+if (window.Entry) {
+  onEntryLoad()
+}
+else {
+  entryInfo.script.addEventListener('load', onEntryLoad)
+}
+
+function onEntryLoad() {
+  // TODO: 여기에 이벤트 추가
+}
+
+
+
+
