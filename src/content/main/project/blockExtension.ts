fac2ea2e6c09fc99cd771e7c869cb3dc917206a4
CouldBeAncient <chebotaaaaaa@gmail.com>
2025-09-30 20:45:32 +0900

    feat(wip): add block schema generator

--- src/content/main/project/blockExtension.ts
+++ src/content/main/project/blockExtension.ts
@@ -43,7 +43,7 @@ export function makeBlockSchema(block: LooseBlockSchema): BlockSchema {
   if (!('type' in block))
     throw new Error("블럭에 type 속성이 없습니다")
 
-  const resultBlock = {...defaultSchema, ...block}
+  const resultBlock = { ...defaultSchema, ...block }
 
   if (!resultBlock.id)
     resultBlock.id = generateHash()
@@ -52,7 +52,7 @@ export function makeBlockSchema(block: LooseBlockSchema): BlockSchema {
 
   // 내용물 복사
   resultBlock.params = [...resultBlock.params]
-  
+
   for (let i = 0; i < resultBlock.params.length; i++) {
     const param = resultBlock.params[i]
     if (param != null && typeof param === 'object' && typeof param.type === 'string') {
@@ -88,5 +88,125 @@ export class BlockExtensionManager {
 
   makePlaceholderFunction(id: string, content?: BlockSchema, result?: BlockSchema) {
 
+  }
+}
+
+type BlockSchemaGeneratorProxy = {[key: string]: (...args: any[]) => BlockSchemaGeneratorToken}
+
+class BlockSchemaGeneratorToken {
+  type: string
+  params: any[]
+  statements: BlockSchemaGeneratorToken[][]
+
+  constructor(type: string, params: any[]) {
+    this.type = type
+    this.params = params
+    this.statements = []
+  }
+
+  statement(callback: (block: BlockSchemaGeneratorProxy) => void) {
+    this.statements.push(new ThreadSchemaGenerator(callback).tokens)
+  }
+
+  export(): BlockSchema {
+    const looseSchema: LooseBlockSchema = {
+      type: this.type,
+      params: this.params.map(x => x instanceof BlockSchemaGeneratorToken ? x.export() : x),
+      statements: this.statements.map(x => x.map(y => y.export()))
+    }
+
+    return makeBlockSchema(looseSchema)
+  }
+}
+
+abstract class BlockSchemaGeneratorBase {
+  protected static blockHandler(this: BlockSchemaGeneratorBase, data: BlockSchemaGeneratorToken[], type: string, ...params: any[]) {
+    params.forEach(param => {
+      if (param instanceof BlockSchemaGeneratorToken) {
+        const idx = data.indexOf(param)
+
+        if (idx !== -1)
+          data.splice(idx, 1)
+      }
+    })
+
+    const token = new BlockSchemaGeneratorToken(type, params)
+
+    data.push(token)
+
+    return token
+  }
+
+  protected makeProxyHandler(data: BlockSchemaGeneratorToken[]) {
+    const handler: ProxyHandler<Object> = {}
+    handler.get = (target, prop, receiver) => {
+      if (typeof prop === 'symbol')
+        throw new TypeError("string만 인자로 받을 수 있습니다")
+
+      return (this.constructor as typeof BlockSchemaGeneratorBase).blockHandler.bind(this, data, prop)
+    }
+
+    return handler
+  }
+
+  // 구현은 필수, 타입은 자유
+  abstract export(...arg: any[]): unknown
+}
+
+export class BlockSchemaGenerator extends BlockSchemaGeneratorBase {
+  token: BlockSchemaGeneratorToken
+  constructor(callback: (block: BlockSchemaGeneratorProxy) => BlockSchemaGeneratorToken) {
+    super()
+
+    const _data: BlockSchemaGeneratorToken[] = []
+
+    const { proxy, revoke } = Proxy.revocable({}, this.makeProxyHandler(_data))
+    this.token = callback(proxy as BlockSchemaGeneratorProxy)
+
+    revoke()
+  }
+
+  export(): BlockSchema {
+    return this.token.export()
+  }
+}
+
+export class ThreadSchemaGenerator extends BlockSchemaGeneratorBase {
+  tokens: BlockSchemaGeneratorToken[]
+  constructor(callback: (block: BlockSchemaGeneratorProxy) => void) {
+    super()
+
+    this.tokens = []
+
+    const { proxy, revoke } = Proxy.revocable({}, this.makeProxyHandler(this.tokens))
+    callback(proxy as BlockSchemaGeneratorProxy)
+
+    revoke()
+  }
+
+  export(): BlockSchema[] {
+    return this.tokens.map(x => x.export())
+  }
+}
+
+export class FunctionSchemaGenerator extends BlockSchemaGeneratorBase {
+  tokens: BlockSchemaGeneratorToken[]
+  resultToken: BlockSchemaGeneratorToken
+  constructor(callback: (block: BlockSchemaGeneratorProxy) => BlockSchemaGeneratorToken) {
+    super()
+
+    this.tokens = []
+
+    const { proxy, revoke } = Proxy.revocable({}, this.makeProxyHandler(this.tokens))
+    this.resultToken = callback(proxy as BlockSchemaGeneratorProxy)
+
+    this.tokens.splice(this.tokens.indexOf(this.resultToken), 1)
+
+    revoke()
+  }
+
+  export(): EntryFunctionSchema[] {
+    // TODO: WIP
+    return this.tokens.map(x => x.export())
   }
 }
